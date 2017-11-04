37c12782c735794154d7653de02675aecf6adb9e
cmccvic <liu.cmri@gmail.com>
2017-11-04 01:17:04 -0400

    0to5

--- run_java.sh
+++ run_java.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # try different Mu with data/p1/test.txt concatenated with data/p2/unlabeled_20news.txt
-for mu in  0.0 0.1 0.2
+for mu in  0.0 0.1 0.2 0.3 0.4 0.5
 do
 echo "predict $mu"
 java -cp jars/jama.jar:bin/ HMM data/p1/train.txt data/p2/concatenated.txt results/p2/prediction_concatenated results/p2/training_log_concatenated $mu
 done
 
 # evaluate the results
-for mu in 0.0 0.1 0.2
+for mu in 0.0 0.1 0.2 0.3 0.4 0.5
 do
 echo "evaluate $mu"
 java -cp bin/ Evaluator data/p1/test.txt results/p2/prediction_${mu}.txt
